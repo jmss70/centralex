a29394b4add91d4d1ce6ddc40ea6b43f14517c5a
Jose Maria Sanchez <jmss70@gmail.com>
2020-09-01 21:48:40 +0200

    Added more information to each output

--- R/displex.R
+++ R/displex.R
@@ -1,12 +1,12 @@
 #' Zipf law
-displex_zipf_law <- function(w, k=1,a=1,d=0) {
+zipf.law <- function(w, k=1,a=1,d=0) {
   f = k/(seq_along(w)+d)^a
   names(f) <- w
   f
 }
 
 #' Exponential law
-displex_exp_law <- function(w,a=2,k=1,d=0) {
+exponential.law <- function(w,a=2,k=1,d=0) {
   f = k/a^(seq_along(w)+d)
   names(f) <- w
   f
@@ -14,7 +14,7 @@ displex_exp_law <- function(w,a=2,k=1,d=0) {
 
 
 #' Additive reduce law
-displex_additive_law <- function(w,k=1) {
+additive.reduce.law <- function(w,k=1) {
   1 - prod(1-w)^k
 }
 
@@ -51,52 +51,104 @@ read.displex <- function(file) {
 # law    -> law to quantify compatibility with interest center
 # reduce -> law to reduce the valorations of each word in each center to just one number
 # return a data.frame with the field availability added.
-displex_availability <- function(data,law=displex_exp_law, reduce=displex_additive_law) {
+build.availability <- function(data,law=exponential.law, reduce=additive.reduce.law) {
   words        = character()
   availability = numeric()
 
   for (i in seq_along(data$words)) {
     words        = c(words, data$words[i][[1]])
-    availability =c(availability, law(data$words[i][[1]]))
+    availability = c(availability, law(data$words[i][[1]]))
   }
   centers     = rep(data$centers, vapply(data$words, FUN = function(x) length(x), FUN.VALUE = 0L))
-  data.frame(centers=centers, words=words, availability=availability) %>%
+
+  new.data <- data.frame(centers=centers, words=words, availability=availability) %>%
     group_by(centers,words) %>%
-    summarise(availability=reduce(availability)) %>%
-    ungroup()
+    summarise(availability=reduce(availability),freq.abs=length(words),.groups="drop")
+  total_by_center <- new.data %>%
+    group_by(centers) %>%
+    summarise(counttotal=sum(freq.abs), .groups="drop") %>% ungroup()
+  new.data <-
+    left_join(new.data, total_by_center, by="centers") %>%
+    mutate(freq.rel=freq.abs/counttotal) %>%
+    arrange(-freq.rel) %>%
+    group_by(centers) %>%
+    mutate(freq.abs.cum = cumsum(freq.abs), freq.rel.cum = cumsum(freq.rel)) %>%
+    ungroup() %>%
+    arrange(centers,-availability)
+  new.data$order <- unlist(tapply(new.data$words, new.data$centers,function(x) {seq_along(x)}))
+  new.data %>% select(centers,words,order,availability,freq.abs,freq.rel,freq.abs.cum,freq.rel.cum)
 }
 
 # Obtain the availability following the López-Strassburger model
 # return a data.frame with the field availability added.
-displex_lopezstrass_availability <- function(data) {
+build.lopezstrass.availability <- function(data) {
   # Máxima posición alcanzada
   n <- max(vapply(data$words, function(x) {length(x)}, FUN.VALUE=1L))
   # Número total de hablantes
   N <- length(unique(data$users))
 
-  displex_availability(data   = data,
-                       law    = function(w) {displex_exp_law(w,a=exp(2.3/(n-1)),d=-1)},
-                       reduce = function(x) {sum(x) / N})
+  build.availability(data   = data,
+                     law    = function(w) {exponential.law(w,a=exp(2.3/(n-1)),d=-1)},
+                     reduce = function(x) {sum(x) / N})
 }
 
 # Obtain the availability following the Ávila-Sánchez model
 # returns a data.frame with the field availability added.
-displex_avilasanchez_availability <- function(data, k=1) {
-  displex_availability(d,
-                       law=function(x) {displex_zipf_law(x,k=k,d=1)},
-                       reduce=displex_additive_law)
+build.avilasanchez.availability <- function(data, k=1) {
+  build.availability(data,
+                     law=function(x) {zipf.law(x,k=k,d=1)},
+                     reduce=additive.reduce.law)
 }
 
 # Sugeno integral of the fuzzy set d, respect fuzzy measure g
 # Returns the value of the integral
 
-fuzzy.expected.value <- function(d, g=function(x) {length(x)/length(d)}) {
+fuzzy.expected.value <- function(d, g=function(x) {length(x)/length(d)}, h=function(x) {sqrt(x)}, level=0) {
+  ng <- function(x) {
+    res <- g(x)
+    for (i in seq(length.out=level)) {
+      res = h(res)
+    }
+    res
+  }
+
   # Calculate by function, and the levels for alpha
   levels <- sort(unique(d), decreasing=TRUE)
   # Determine alpha cuts and its measure
-  gs <- sapply(levels,function(x) {g(d[d >= x])})
+  gs <- sapply(levels,function(x) {ng(d[d >= x])})
   res  <- cbind(levels,gs)
   res <- max(apply(res,1,min))
   res
 }
 
+# Label each word in each interest center in order to build levels of centrality
+# data -> Availability data.frame as returned by build.avilability
+# levels -> numbers of levels to clasify vocabulary
+# returns -> a new data.frame, similar to input, but with labeled levels
+classify.availability.levels <- function(data, levels=6) {
+  #new.data <- data %>% select(centers,words,order,availability)
+  new.data <- data
+  new.data$cutlevel = 0
+  new.data$level = 0
+  levels <- 6
+  for (i in seq(length.out = levels)) {
+    cuts <- tapply(new.data$availability,new.data$centers,function(x) {fuzzy.expected.value(x,level=i)})
+    cuts <- data.frame(cuts=cuts,centers=names(cuts))
+    new.data <- left_join(new.data, cuts, by="centers")
+    new.data$level[new.data$availability >= new.data$cuts] <- i
+    new.data$cutlevel[new.data$availability >= new.data$cuts] <- new.data$cuts[new.data$availability >= new.data$cuts]
+    new.data <- new.data %>% select(centers,words,order,availability,level,cutlevel,freq.abs,freq.rel,freq.abs.cum,freq.rel.cum)
+  }
+  new.data %>% arrange(-availability,centers)
+}
+
+# Reduce data.frame to a
+build.availability.levels <- function(data) {
+  mt <- tapply(data$words, list(data$centers,data$level),c,simplify=FALSE)
+  dmt <- data.frame(mt)
+  names(dmt) <- colnames(mt)
+  dmt$centers <- rownames(dmt)
+  dmt <- dmt %>% pivot_longer(-centers,names_to="levels", values_to="words") %>% mutate(levels=strtoi(levels))
+  dmt$count <- lapply(dmt$words, function(x) {length(unlist(x))}) %>% unlist
+  dmt %>% select(centers,levels,count,words) %>% arrange(centers,-levels)
+}
